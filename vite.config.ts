341b4fb13f92818e85c5c3ea883a6f0665689334
Vartec <misticmvm@gmail.com>
2025-09-08 21:18:41 +0300

    fix: update Vite React plugin to version 5.0.2

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 
 import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
